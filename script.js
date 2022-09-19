3533e011e3707c25342f88ffbb0b8ff267bd829d
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2022-09-19 18:20:53 +0100

    Basic CSS styling

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const container = document.querySelector('#container');
 
 for (let i = 0; i < 256; i++) {
     const div = document.createElement('div');
-    div.setAttribute('id', 'canvas-grid-box');
+    div.classList.add('canvas-grid-box');
     container.appendChild(div);
-}
+};
+
+const gridBoxes = document.querySelectorAll('#container > div');
+
+console.log(gridBoxes);
+
+gridBoxes.forEach
