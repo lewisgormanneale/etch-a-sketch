0ccd7dbf8a7ecf9b4498092b9c015e980ef0c7fe
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2022-10-01 16:38:01 +0100

    beginning to define standard, changeable makeGrid function

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-const container = document.querySelector('#container');
+const startColor = '#FFFFFF'
+const startSize = 16;
 
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.classList.add('canvas-grid-box');
-    container.appendChild(div);
+let currentColor = startColor;
+let currentSize = startSize;
+
+const container = document.querySelector('#container');
+let rainbowMode = false;
+
+function makeGrid(size) {
+    for (let i = 0; i < (size * size); i++) {
+        const div = document.createElement('div');
+        div.classList.add('canvas-grid-box');
+        container.appendChild(div);
+        gridBoxes = document.querySelectorAll('#container > div');
+        gridBoxes[i].addEventListener('click', fillIn);
+    };
 };
 
-const gridBoxes = document.querySelectorAll('#container > div');
+function pickRainbowColor() {
+    let colorArray = ['#ef3550', '#f48fb1', '#7e57c2', '#2196f3', '#26c6da', '#43a047', '#eeff41', '#f9a825', '#ff5722']
+    let randomColor = colorArray[Math.floor( Math.random() * colorArray.length)];//pick random value from array
+    return randomColor;
+};
 
-console.log(gridBoxes);
+function fillIn() {
+    let fillColor = '#000000';
+    if (rainbowMode = true) {
+        fillColor = pickRainbowColor();
+    }
+    let col = document.getElementById
+    gridBoxes.style.backgroundColor = fillColor;
+    console.log(fillColor);
+}
 
-gridBoxes.forEach
+function resetGrid() {
+
+}
+
+window.addEventListener('load', makeGrid(16));
